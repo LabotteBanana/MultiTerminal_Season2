da2da1f4c2f4f7563469b1d2939a20c9c544295b
CheeseCraftKr <kyus1993@naver.com>
2017-04-28 18:57:41 +0900

    빈도정리

--- MultiTerminal/MultiTerminal/frequency.cs
+++ MultiTerminal/MultiTerminal/frequency.cs
@@ -15,6 +15,23 @@ namespace MultiTerminal
             this.ReceiveWindowbox = ReceiveWindowBox;
         }
 
+        //이름만 가져와서 선택 체크박스로 선택 가능하게.
+        private void getConnection()
+        {
+
+        }
+        //분석할 때 시간 계산할 메소드
+        private void getAllTime()
+        {
+
+        }
+        //빈도를 증가시킬 메소드
+        private void getFrequency()
+        {
+
+        }
+        //그래프를 그릴 메소드
+        //2차원 배열정의 및 구현 필요
         private void graphAnalysis()
         {
             
