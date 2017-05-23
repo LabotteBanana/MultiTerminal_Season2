070c2933850d07c5e3627ce4549e38c76c3fa79b
MyHome <MyHome@DESKTOP-8SBESRU>
2017-05-23 19:45:42 +0900

    000

--- Master Multiterminal/MultiTerminal/udpServer.cs
+++ Master Multiterminal/MultiTerminal/udpServer.cs
@@ -13,29 +13,24 @@ namespace MultiTerminal
     public class udpServer
     {
         MainForm main = null;
-        private string nowPortSt;
+
         private Dictionary<int, EndPoint> m_ClientEP = new Dictionary<int, EndPoint>();
-        //private int m_ClientCount;
+        private int m_ClientCount;
+        private int m_DisConnectNum;
         public Socket server;
         private IPEndPoint EP;
         public int port;
         public bool m_isConnected = false;
         private static Thread th = null;
-        private List<string> m_PortList = new List<string>();
-        //public Dictionary<int, bool> m_bSendList = new Dictionary<int, bool>();
-        //public bool m_bRecv = true;
-        private GridView gridview = null;
-        private List<GridView> gridList = null;
-
-        public void Connect(MainForm form, int Port, GridView Gridview, List<GridView> GridList)
+        public Dictionary<int, bool> m_bSendList = new Dictionary<int, bool>();
+        public bool m_bRecv = true;
+        public void Connect(MainForm form, int Port)
         {
             try
             {
                 main = form;
                 EP = new IPEndPoint(IPAddress.Any, Port);
                 port = Port;
-                gridList = GridList;
-                gridview = Gridview;
                 server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
                 server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
@@ -71,9 +66,9 @@ namespace MultiTerminal
 
                 if (server != null)
                 {
-                    for (int i = 0; i < gridList.Count; i++)
+                    for (int i = 0; i < m_ClientCount; i++)
                     {
-                        //if (m_bSendList[i] == true)
+                        if (m_bSendList[i] == true)
                         {
                             server.SendTo(data, m_ClientEP[i]);
                         }
@@ -112,72 +107,65 @@ namespace MultiTerminal
                         int recvi = server.ReceiveFrom(recv, ref remoteEP);
                         //어느 클라이언트에서 보냈는지 판단 가능
                         string recvMsg = Encoding.Default.GetString(recv);
-                        bool connectMsg = recvMsg.StartsWith("서버로 접속");
-                        bool disconnectMsg = recvMsg.StartsWith("서버로 해제");
+                        bool aaaa = recvMsg.StartsWith("서버로 접속");
                         if (recvi > 0) //메시지 도달
                         {
                             m_isConnected = true;
                             String ClientInfo = remoteEP.ToString();
                             int StrStart = ClientInfo.IndexOf(':');
                             String PortStr = ClientInfo.Substring(StrStart + 1); //접속해온 곳의 Port번호 확인
-                            nowPortSt = PortStr;
 
-                            if (connectMsg == true && recvi == 11)
+
+                            if (aaaa == true && recvi == 11)
                             {
-                                ///추가
-                                //System.Windows.Forms.DataGridViewCheckBoxColumn udpRx = main.Rx;
-                                m_ClientEP.Add(gridList.Count, remoteEP);
-                                //m_bSendList.Add(gridList.Count, true);
+                                System.Windows.Forms.DataGridViewCheckBoxColumn udpRx = main.Rx;
+                                m_ClientEP.Add(m_ClientCount++, remoteEP);
                                 if (main.InvokeRequired)
                                 {
                                     // 그리드뷰 객체에 적용,   타입형태(시리얼,UDP..), 타입의 순번도 그리드 객체로 슝들어감.
-                                    main.Invoke(new Action(() => gridview = new GridView(gridList.Count, PortStr.ToString(), "UDP Client", gridList.Count)));
-                                    main.Invoke(new Action(() => main.DrawGrid(gridview.MyNum, gridview.Type, gridview.Portname, gridview.Time)));
-                                    main.Invoke(new Action(() => gridList.Add(gridview)));
-                                    m_PortList.Add(PortStr.ToString());
-                                    //main.Invoke(new Action(() => udpRx.ReadOnly = true));
+                                    main.Invoke(new Action(() => main.gridview= new GridView(main.GridList.Count, PortStr.ToString(), "UDPClient", m_ClientCount)));
+                                    main.Invoke(new Action(() => main.DrawGrid(main.gridview.MyNum, main.gridview.Type, main.gridview.Portname, main.gridview.Time)));
+                                    main.Invoke(new Action(() => main.GridList.Add(main.gridview)));
+                                    main.Invoke(new Action(() => udpRx.ReadOnly = true));
                                 }
                                 else
                                 {
-                                    gridview = new GridView(gridList.Count, PortStr.ToString(), "UDP Client", gridList.Count);
-                                    main.DrawGrid(gridview.MyNum, gridview.Type, gridview.Portname, gridview.Time);
-                                    gridList.Add(gridview);
-                                    m_PortList.Add(PortStr.ToString());
+                                    main.gridview = new GridView(main.GridList.Count, PortStr.ToString(), "UDPClient", m_ClientCount);
+                                    main.DrawGrid(main.gridview.MyNum, main.gridview.Type, main.gridview.Portname, main.gridview.Time);
+                                    main.GridList.Add(main.gridview);
+                                    udpRx.ReadOnly = true;
+                                }
+                            }
+                            if (main.RowIndex >= 0 && main.GridList[main.RowIndex].RxCheckedState == true)
+                            {
+                                for (int j = 0; j < m_ClientCount; j++)
+                                {
+                                    String Info = m_ClientEP[j].ToString();
+                                    int Start = Info.IndexOf(':');
+                                    String Port = Info.Substring(Start + 1); //접속해온 곳의 Port번호 확인
 
-                                    //udpRx.ReadOnly = true;
-                                }
-                            }
-                            if (disconnectMsg == true && recvi == 11)
-                            {
-                                for (int i = 0; i < m_PortList.Count; i++)
-                                {
-                                    if (PortStr == m_PortList[i])
+                                    m_DisConnectNum = j;
+                                    if (m_bRecv == true && String.Compare(Port, PortStr) == 0)
                                     {
-                                        m_ClientEP.Remove(i);
-                                        main.RemoveGridforIP(m_PortList[i]);
-                                    }
-                                }
-                            }
-                            if (main.RowIndex >= 0)
-                            {
-                                {
-                                    if (main.InvokeRequired)
-                                    {
-                                        main.Invoke(new Action(() => main.ReceiveWindowBox.Text += "수신 :" + main.GetTimer() + recvMsg + "\n"));
-                                        main.Invoke(new Action(() => main.ReceiveWindowBox.Text += "" + Environment.NewLine));
-                                        main.Invoke(new Action(() => main.ReceiveWindowBox.SelectionStart = main.ReceiveWindowBox.Text.Length));
-                                        main.Invoke(new Action(() => main.ReceiveWindowBox.ScrollToCaret()));
+                                        ///이부분 문제
+                                        if (main.InvokeRequired)
+                                        {
+                                            main.Invoke(new Action(() => main.ReceiveWindowBox.Text += "수신 :" + main.GetTimer() + recvMsg + "\n"));
+                                            main.Invoke(new Action(() => main.ReceiveWindowBox.Text += "" + Environment.NewLine));
+                                            main.Invoke(new Action(() => main.ReceiveWindowBox.SelectionStart = main.ReceiveWindowBox.Text.Length));
+                                            main.Invoke(new Action(() => main.ReceiveWindowBox.ScrollToCaret()));
+                                        }
+                                        else
+                                        {
+                                            main.ReceiveWindowBox.Text += "수신 :" + main.GetTimer() + recvMsg + "\n";
+                                            main.ReceiveWindowBox.Text += "" + Environment.NewLine;
+                                            main.ReceiveWindowBox.SelectionStart = main.ReceiveWindowBox.Text.Length;
+                                            main.ReceiveWindowBox.ScrollToCaret();
+                                        }
                                     }
                                     else
-                                    {
-                                        main.ReceiveWindowBox.Text += "수신 :" + main.GetTimer() + recvMsg + "\n";
-                                        main.ReceiveWindowBox.Text += "" + Environment.NewLine;
-                                        main.ReceiveWindowBox.SelectionStart = main.ReceiveWindowBox.Text.Length;
-                                        main.ReceiveWindowBox.ScrollToCaret();
-                                    }
+                                        continue;
                                 }
-                                //else
-                                //continue;
                             }
                         }
                     }
@@ -185,21 +173,20 @@ namespace MultiTerminal
             }
             catch (SocketException ex)
             {
-                //클라이언트의 접속종료를 알게되는 시점 (보내봐야안다.)
+                //클라이언트가 접속종료를 알게되는 시점 (보내봐야안다.)
                 if (ex.ErrorCode == 10054)
                 {
-                    System.Windows.Forms.MessageBox.Show(nowPortSt + "포트 클라이언트에서 연결을 종료했습니다.");
+                    System.Windows.Forms.MessageBox.Show(m_DisConnectNum + "번 클라이언트에서 연결을 종료했습니다.");
+                    m_ClientEP.Remove(m_DisConnectNum);
 
-                    //System.Windows.Forms.MessageBox.Show(m_DisConnectNum + "번 클라이언트에서 연결을 종료했습니다.");
-                    //m_ClientEP.Remove(m_DisConnectNum);
-                    //gridList.RemoveAt(m_DisConnectNum);
-
-                    //m_bSendList.Remove(m_DisConnectNum);
-                    //main.PortListGrid.Rows.RemoveAt(m_DisConnectNum);
-                    //main.PortListGrid.Update();
-                    //main.PortListGrid.Refresh();
+                    m_bSendList.Remove(m_DisConnectNum);
+                    main.GridList.RemoveAt(m_DisConnectNum);
+                    main.PortListGrid.Rows.RemoveAt(m_DisConnectNum);
+                    main.PortListGrid.Update();
+                    main.PortListGrid.Refresh();
 
                     ///
+                    m_ClientCount--;
                 }
                 else
                 {
@@ -214,41 +201,26 @@ namespace MultiTerminal
                 System.Windows.Forms.MessageBox.Show("기타에러 " + lineNum + "에서 발생" + ex.Message);
             }
         }
-        public void DisConnect(int i)
-        {
-            if (server != null)
-            {
-                IPEndPoint Sender = new IPEndPoint(IPAddress.Any, 0);
-                EndPoint remoteEP = (EndPoint)Sender;
-                String ClientInfo = remoteEP.ToString();
-                int StrStart = ClientInfo.IndexOf(':');
-                String PortStr = ClientInfo.Substring(StrStart + 1); //접속해온 곳의 Port번호 확인
-                if (PortStr == m_PortList[i])
-                {
-                    m_ClientEP.Remove(i);
-                    main.RemoveGridforIP(m_PortList[i]);
-                }
-            }
-        }
-
         public void DisConnect()
         {
             if (server != null)
             {
-                //int row;
-                //m_isConnected = false;
-                //string udpclient = "UDPClient";
-                ////그리드 리스트의 n번째 인덱스
-                //if (main.PortListGrid.Columns[2].ToString() == udpclient)
-                //{
-                //    row = main.PortListGrid.CurrentCell.RowIndex;
-                //    main.GridList.RemoveAt(row);
-                //    main.PortListGrid.Rows.RemoveAt(row);
-                //}
-                //main.PortListGrid.Update();
-                //main.PortListGrid.Refresh();
+                int row;
+                m_isConnected = false;
+                string udpclient = "UDPClient";
+                //그리드 리스트의 n번째 인덱스
+                if (main.PortListGrid.Columns[2].ToString() == udpclient)
+                {
+                    row = main.PortListGrid.CurrentCell.RowIndex;
+                    main.GridList.RemoveAt(row);
+                    main.PortListGrid.Rows.RemoveAt(row);
+                }
+                main.PortListGrid.Update();
+                main.PortListGrid.Refresh();
 
-                //main.RemoveGridforIP(Ip);
+                //main.GridList.Remove(main.GridList[main.RowIndex]);
+
+                //server.Shutdown(SocketShutdown.Both);
                 server.Close();
             }
         }
